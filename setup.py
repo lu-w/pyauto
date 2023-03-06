52653c5e32daff8ebd89c9236ea9414fdcfbb709
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-03-06 15:05:36 +0100

    Adds setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name="pyauto",
+    version="0.1",
+    description="Python module for accessing A.U.T.O. based on owlready2",
+    author ="Lukas Westhofen",
+    author_email="lukas.westhofen@dlr.de",
+    py_modules=["pyauto"],
+    install_requires=[
+        "owlready2",
+        "matplotlib",
+        "mpld3",
+        "shapely",
+        "numpy",
+        "screeninfo",
+        "tqdm"
+    ]
+)
