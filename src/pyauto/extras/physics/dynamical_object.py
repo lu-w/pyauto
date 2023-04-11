6e22d69982b6890fae5e0e5d869d1fecc41f8ab0
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-04-11 17:33:05 +0200

    Decouples intersecting path function from augmentation functionality
    Enables to re-use intersecting path functionality in other contexts.

--- src/pyauto/extras/physics/dynamical_object.py
+++ src/pyauto/extras/physics/dynamical_object.py
@@ -1,11 +1,14 @@
 import math
 import numpy
+import sympy
 import owlready2
 
 from shapely import wkt
 from owlready2_augmentator import augment, augment_class, AugmentationType
+
 from ... import auto
 from .spatial_object import _SPATIAL_PREDICATE_THRESHOLD
+from .moving_dynamical_object import _INTERSECTING_PATH_MAX_PET
 
 physics = auto.world.get_ontology(auto.Ontology.Physics.value)
 geosparql = auto.world.get_ontology(auto.Ontology.GeoSPARQL.value)
@@ -87,3 +90,40 @@ with physics:
                 distance = float(p1.distance(p2))
                 if distance <= _SPATIAL_PREDICATE_THRESHOLD:
                     return distance
+
+        def intersects_path_with(self, other: physics.Moving_Dynamical_Object,
+                                 max_pet: float = _INTERSECTING_PATH_MAX_PET) -> tuple[float, float]:
+            """
+            Whether this object has an intersecting path with the given other object.
+            :param other: The other moving dynamical object.
+            :param max_pet: The time interval of PET that classifies intersecting paths as critical.
+            :returns: The times that self and other needs to the intersection point as a tuple, or None, None if there
+                is no intersection point.
+            """
+            if self.has_geometry() and other.has_geometry() and self.has_yaw is not None and other.has_yaw is not None \
+                    and self.has_speed and other.has_speed:
+                p_1 = wkt.loads(self.hasGeometry[0].asWKT[0]).centroid
+                p_2 = wkt.loads(other.hasGeometry[0].asWKT[0]).centroid
+                p_self = sympy.Point(p_1.x, p_1.y)
+                p_other = sympy.Point(p_2.x, p_2.y)
+                if p_self != p_other:
+                    self_yaw = self.has_yaw
+                    other_yaw = other.has_yaw
+                    if self.has_speed < 0:
+                        self_yaw = (self.has_yaw + 180) % 360
+                    if other.has_speed < 0:
+                        other_yaw = (other.has_yaw + 180) % 360
+                    p_self_1 = sympy.Point(p_1.x + math.cos(math.radians(self_yaw)),
+                                              p_1.y + math.sin(math.radians(self_yaw)))
+                    p_other_1 = sympy.Point(p_2.x + math.cos(math.radians(other_yaw)),
+                                               p_2.y + math.sin(math.radians(other_yaw)))
+                    self_path = sympy.geometry.Ray(p_self, p_self_1)
+                    other_path = sympy.geometry.Ray(p_other, p_other_1)
+                    p_cross = sympy.geometry.intersection(self_path, other_path)
+                    if len(p_cross) > 0:
+                        d_self = p_cross[0].distance(p_self)
+                        d_other = p_cross[0].distance(p_other)
+                        t_self = float(d_self) / self.has_speed
+                        t_other = float(d_other) / other.has_speed
+                        return (t_self, t_other)
+            return (None, None)
--- src/pyauto/extras/physics/moving_dynamical_object.py
+++ src/pyauto/extras/physics/moving_dynamical_object.py
@@ -5,10 +5,11 @@ import owlready2
 from shapely import geometry, wkt
 import sympy.geometry
 from owlready2_augmentator import augment, augment_class, AugmentationType
+
 from ... import auto
 
 _INTERSECTING_PATH_THRESHOLD = 8   # s, the time interval in which future intersecting paths shall be detected
-_INTERSECTING_PATH_MAX_PET = 3     # s, the time interval in which future intersecting paths shall be detected
+_INTERSECTING_PATH_MAX_PET = 3     # s, the time interval of PET that classifies intersecting paths as critical
 _HIGH_REL_SPEED_THRESHOLD = 0.25   # rel., the relative difference in total speed in which CP 150 will be augmented
 _DEFAULT_SPEED_LIMIT = 50          # km/h, the default speed limit that is assumed
 _DEFAULT_MAX_SPEED = 50            # km/h, the default speed maximum speed that is assumed
@@ -20,42 +21,20 @@ with physics:
 
     @augment_class
     class Moving_Dynamical_Object(owlready2.Thing):
+
         @augment(AugmentationType.OBJECT_PROPERTY, "has_intersecting_path")
-        def intersects_path_with(self, other: physics.Moving_Dynamical_Object) -> bool:
+        def augment_intersecting_paths(self, other: physics.Moving_Dynamical_Object):
             """
             Whether this object has an intersecting path with the given other object.
             :param other: The other moving dynamical object.
             :returns: True iff. the intersecting path condition is satisfied.
             """
-            if self.has_geometry() and other.has_geometry() and self.has_yaw is not None and other.has_yaw is not None \
-                    and self.has_speed and other.has_speed:
-                p_1 = wkt.loads(self.hasGeometry[0].asWKT[0]).centroid
-                p_2 = wkt.loads(other.hasGeometry[0].asWKT[0]).centroid
-                p_self = geometry.Point(p_1.x, p_1.y)
-                p_other = geometry.Point(p_2.x, p_2.y)
-                if p_self != p_other:
-                    self_yaw = self.has_yaw
-                    other_yaw = other.has_yaw
-                    if self.has_speed < 0:
-                        self_yaw = (self.has_yaw + 180) % 360
-                    if other.has_speed < 0:
-                        other_yaw = (other.has_yaw + 180) % 360
-                    p_self_1 = geometry.Point(p_1.x + math.cos(math.radians(self_yaw)),
-                                              p_1.y + math.sin(math.radians(self_yaw)))
-                    p_other_1 = geometry.Point(p_2.x + math.cos(math.radians(other_yaw)),
-                                               p_2.y + math.sin(math.radians(other_yaw)))
-                    self_path = sympy.geometry.Ray(p_self, p_self_1)
-                    other_path = sympy.geometry.Ray(p_other, p_other_1)
-                    p_cross = sympy.geometry.intersection(self_path, other_path)
-                    if len(p_cross) > 0:
-                        d_self = geometry.Point.distance(p_cross[0], p_self)
-                        d_other = geometry.Point.distance(p_cross[0], p_other)
-                        t_self = float(d_self) / self.has_speed
-                        t_other = float(d_other) / other.has_speed
-                        return t_self + t_other < _INTERSECTING_PATH_THRESHOLD and \
-                               abs(t_self - t_other) < _INTERSECTING_PATH_MAX_PET
-                    else:
-                        return False
+            t_self, t_other = self.intersects_path_with(other)
+            if t_self is None or t_other is None:
+                return False
+            else:
+                return t_self + t_other < _INTERSECTING_PATH_THRESHOLD and \
+                    abs(t_self - t_other) < _INTERSECTING_PATH_MAX_PET
 
         @augment(AugmentationType.OBJECT_PROPERTY, "CP_163")
         def has_high_relative_speed_to(self, other: physics.Moving_Dynamical_Object):
