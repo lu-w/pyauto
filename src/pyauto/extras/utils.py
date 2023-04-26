e8a3b3df721724903494070014db00aa8ff2e661
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-04-26 10:20:35 +0200

    Get closest points from yaw can now be computed for a given field of relevance

--- src/pyauto/extras/utils.py
+++ src/pyauto/extras/utils.py
@@ -37,45 +37,74 @@ def split_polygon_into_boundaries(p: Polygon, splitting_points: tuple[Point, Poi
     return left, right, front, back
 
 
-def get_closest_point_from_yaw(ls: LineString, p: Point, yaw: float | int) -> Point:
+def get_closest_point_from_yaw(ls: LineString, p: Point, yaw: float | int, angle: float | int=180) -> Point:
     """
     Finds the nearest point in the line string based on the given point and yaw (i.e., only examines points within the
     180° field in which the yaw points from the given point).
     :param ls: The line string to examine
     :param p: The point for which to get the closest point
     :param yaw: The viewing angle to consider when looking for the closest point.
+    :param angle: An angle (in °) that depicts the viewing angle from the yaw in which the closest points are searched.
     :returns: The closes point of the line string based on the yaw or None if there is no point of ls in the 180° field.
     """
-    yaw_p = (yaw - 90) % 360
-    p_2 = Point(math.cos(math.radians(yaw_p)) + p.x, math.sin(math.radians(yaw_p)) + p.y)
-    if p_2.x - p.x != 0:
-        m = (p_2.y - p.y) / (p_2.x - p.x)
-        b = p.y - m * p.x
-    else:  # case: yaw is 0° or 180°
-        m = None
-        b = None
-    relevant_points = []
-    for lp in ls.coords:
-        if m is not None:
-            div_y = m * lp[0] + b
-            if 0 < yaw <= 180:
-                if lp[1] >= div_y:
-                    relevant_points.append(lp)
+
+    def _get_relevant_points(yaw, p, ls, angle):
+        """
+        Helper method to determine the points of ls in front of yaw at point p, where in front of is defined by the
+        given angle (which is understood relative to the yaw and draws a 'separating line').
+        :return: A list of relevant points.
+        """
+        yaw_p = (yaw + angle) % 360
+        p_2 = Point(math.cos(math.radians(yaw_p)) + p.x, math.sin(math.radians(yaw_p)) + p.y)
+        if p_2.x - p.x != 0:
+            m = (p_2.y - p.y) / (p_2.x - p.x)
+            b = p.y - m * p.x
+        else:  # case: yaw - angle is 90° or 270°
+            m = None
+            b = None
+        relevant_points = []
+        for lp in ls.coords:
+            if m is not None:
+                div_y = m * lp[0] + b
+                if angle < 0:  # right of yaw
+                    if 90 < yaw_p <= 270:
+                        if lp[1] <= div_y:
+                            relevant_points.append(lp)
+                    else:
+                        if lp[1] >= div_y:
+                            relevant_points.append(lp)
+                else:  # left of yaw:
+                    if 90 < yaw_p <= 270:
+                        if lp[1] >= div_y:
+                            relevant_points.append(lp)
+                    else:
+                        if lp[1] <= div_y:
+                            relevant_points.append(lp)
             else:
-                if lp[1] < div_y:
-                    relevant_points.append(lp)
-        else:
-            if yaw == 0:
-                if p.x <= lp[0]:
-                    relevant_points.append(lp)
-            elif yaw == 180:
-                if p.x > lp[0]:
-                    relevant_points.append(lp)
-            else:
-                logger.warning("Assumed yaw to be 0 or 180, but this does not seem to hold.")
+                if angle < 0:  # right of yaw
+                    if yaw_p == 90:
+                        if lp[0] <= p.x:
+                            relevant_points.append(lp)
+                    elif yaw_p == 270:
+                        if lp[0] >= p.x:
+                            relevant_points.append(lp)
+                else:  # left of yaw
+                    if yaw_p == 90:
+                        if lp[0] >= p.x:
+                            relevant_points.append(lp)
+                    elif yaw_p == 270:
+                        if lp[0] <= p.x:
+                            relevant_points.append(lp)
+        return relevant_points
+
+    rel_points_1 = _get_relevant_points(yaw, p, ls, angle / 2)
+    if not math.isclose(angle, 180):
+        rel_points_2 = _get_relevant_points(yaw, p, ls, - angle / 2)
+    else:
+        rel_points_2 = rel_points_1
     p_closest = None
     closest = None
-    for rp in relevant_points:
+    for rp in set(rel_points_1).intersection(set(rel_points_2)):
         dist = p.distance(Point(rp))
         if p_closest is None or dist < closest:
             p_closest = Point(rp)
