30505b0002001767b91fc6d779c5fa67a1192e55
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-05-03 17:44:50 +0200

    Updates documentation of util functions

--- src/pyauto/extras/utils.py
+++ src/pyauto/extras/utils.py
@@ -6,8 +6,7 @@ from shapely.geometry import Polygon, LineString, Point
 logger = logging.getLogger(__name__)
 
 
-def split_polygon_into_boundaries(p: Polygon, splitting_points: tuple[Point, Point, Point] = None) \
-        -> tuple[LineString, LineString, LineString, LineString]:
+def split_polygon_into_boundaries(p: Polygon) -> tuple[LineString, LineString, LineString, LineString]:
     """
     Splits a polygon into its left, right, front, and back boundaries (as a tuple of new LineString).
     Assumptions: The polygon has the same number of points on each side, and its ends (i.e, front and back) are
@@ -15,15 +14,10 @@ def split_polygon_into_boundaries(p: Polygon, splitting_points: tuple[Point, Poi
     half, the front part will be from there to the beginning point of p's second half, and the part of the beginning of
     p's second half to the last point of p is the left part. The back connects the first and last point of p.
     Note: assumes at least four, and an even number of points present in p's boundary.
-    :param p: The polygon to split
-    :param splitting_points: If the above assumption does not hold, this is the set of points to split the polygon at.
-        Right part will be from the beginning of p to the first splitting point, the front part will be from there to
-        the second splitting point, and the part up to the last splitting point will be the left part. The back connects
-        the first point of p with the last splitting point.
-    :returns: A tuple of new LineString
+    :param p: The `Polygon` to split
+    :returns: A tuple of new `LineString`s
     """
-    # TODO: implement splitting points
-    coords = p.boundary.coords[:-1]  # last point is the first for a closed polygon - ignore it
+    coords = p.boundary.coords[:-1]  # last point is the first for a polygon - ignore it
     assert(len(coords) >= 4)
     if len(coords) % 2 == 0:
         half = int(len(coords) / 2)
@@ -40,7 +34,7 @@ def split_polygon_into_boundaries(p: Polygon, splitting_points: tuple[Point, Poi
     return left, right, front, back
 
 
-def get_closest_point_from_yaw(ls: LineString, p: Point, yaw: float | int, angle: float | int=180) -> Point:
+def get_closest_point_from_yaw(ls: LineString, p: Point, yaw: float | int, angle: float | int = 180) -> Point:
     """
     Finds the nearest point in the line string based on the given point and yaw (i.e., only examines points within the
     180° field in which the yaw points from the given point).
